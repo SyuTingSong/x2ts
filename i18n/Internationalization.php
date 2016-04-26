42912c37e9714c2a5ffcdad0d102fac4480cebd0
TingSong-Syu <rek@rek.me>
2016-04-26 16:40:02 +0800

    Check if Accept-Language exists before using it

--- i18n/Internationalization.php
+++ i18n/Internationalization.php
@@ -21,32 +21,35 @@ class Internationalization extends Component {
             $class = "\\lang\\$lang";
             return new $class();
         }
-        preg_match_all(
-            '#([\w\-, ]+); ?q=([01]\.\d)#',
-            $_SERVER['HTTP_ACCEPT_LANGUAGE'],
-            $m,
-            PREG_SET_ORDER
-        );
-        usort($m, function ($a, $b) {
-            $a = floatval($a[2]) * 100;
-            $b = floatval($b[2]) * 100;
-            return $b - $a;
-        });
-        $langs = array();
-        foreach ($m as $item) {
-            $group = explode(',', $item[1]);
-            foreach ($group as $lang) {
-                $lang = trim($lang);
-                if (!empty($lang))
-                    $langs[] = str_replace('-', '', ucfirst($lang));
+        if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+            preg_match_all(
+                '#([\w\-, ]+); ?q=([01]\.\d)#',
+                $_SERVER['HTTP_ACCEPT_LANGUAGE'],
+                $m,
+                PREG_SET_ORDER
+            );
+            usort($m, function ($a, $b) {
+                $a = floatval($a[2]) * 100;
+                $b = floatval($b[2]) * 100;
+                return $b - $a;
+            });
+            $langs = array();
+            foreach ($m as $item) {
+                $group = explode(',', $item[1]);
+                foreach ($group as $lang) {
+                    $lang = trim($lang);
+                    if (!empty($lang))
+                        $langs[] = str_replace('-', '', ucfirst($lang));
+                }
+            }
+            foreach ($langs as $lang) {
+                $class = '\\lang\\' . $lang;
+                if (class_exists($class)) {
+                    return new $class();
+                }
             }
         }
-        foreach ($langs as $lang) {
-            $class = '\\lang\\' . $lang;
-            if (class_exists($class)) {
-                return new $class();
-            }
-        }
+
         $class = '\\lang\\' . static::$_conf['default'];
         return new $class();
     }
