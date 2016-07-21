182d8146ecfa2684974305487e6d700daa6b49f6
TingSong-Syu <rek@rek.me>
2016-07-21 20:11:21 +0800

    add x2ts\ReadOnlyException

---
+++ ReadOnlyException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: rek
+ * Date: 2016/7/21
+ * Time: 下午2:32
+ */
+
+namespace x2ts;
+
+/**
+ * Class ReadOnlyException
+ *
+ * @package x2ts
+ */
+class ReadOnlyException extends \Exception {}
