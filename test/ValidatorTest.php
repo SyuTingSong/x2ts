5e0cc646fd9a492368162963a710f8c78a48e2bc
TingSong-Syu <rek@rek.me>
2016-08-17 22:48:07 +0800

    add some more tests for validator

--- test/ValidatorTest.php
+++ test/ValidatorTest.php
@@ -8,6 +8,7 @@
 
 namespace x2ts\test;
 
+use x2ts\validator\StringValidator;
 use x2ts\validator\ValidatorException;
 
 require_once __DIR__ . '/xts.php';
@@ -239,6 +240,193 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase {
             ->validate()
             ->safeVar;
         static::assertEquals($this->testData['string'], $r['string']);
+    }
+
+    /**
+     * @expectedException \x2ts\validator\ValidatorException
+     */
+    public function testStringInvalidContainCI() {
+        XTS::validator($this->testData)
+            ->str('string')
+            ->containCI('word')
+            ->onErrorReport('errors')
+            ->validate();
+    }
+
+    /**
+     * @expectedException \x2ts\validator\ValidatorException
+     */
+    public function testStringNotMatch() {
+        XTS::validator($this->testData)
+            ->str('mobile')
+            ->notMatch('#^\d+$#')
+            ->onErrorReport('error')
+            ->validate();
+    }
+
+    public function testStringStartWith() {
+        $vars = XTS::validator($this->testData)
+            ->str('mobile')
+            ->startWith('138')
+            ->validate()
+            ->safeVar;
+        static::assertEquals($this->testData['mobile'], $vars['mobile']);
+    }
+
+    /**
+     * @expectedException \x2ts\validator\ValidatorException
+     */
+    public function testStringNotStartWith() {
+        $vars = XTS::validator($this->testData)
+            ->str('mobile')
+            ->startWith('130')
+            ->validate();
+    }
+
+    public function testStringCallback() {
+        XTS::validator($this->testData)
+            ->str('string')
+            ->callback(function ($var) {
+                static::assertEquals($this->testData['string'], $var);
+                return true;
+            })
+            ->validate();
+    }
+
+    public function testStringStartWithCI() {
+        $vars = XTS::validator($this->testData)
+            ->str('string')
+            ->startWithCI('HELLO')
+            ->validate()
+            ->safeVar;
+        static::assertEquals($this->testData['string'], $vars['string']);
+    }
+
+    /**
+     * @expectedException \x2ts\validator\ValidatorException
+     */
+    public function testStringStartWithCIFailed() {
+        XTS::validator($this->testData)
+            ->str('string')
+            ->startWithCI('world')
+            ->validate();
+    }
+
+    /**
+     * @param bool   $isValid
+     * @param string $str
+     * @param string $end
+     *
+     * @dataProvider dataForStringEndWith
+     */
+    public function testStringEndWith(bool $isValid, string $str, string $end) {
+        try {
+            (new StringValidator($str))
+                ->endWith($end)
+                ->validate();
+            static::assertTrue($isValid);
+        } catch (ValidatorException $e) {
+            static::assertFalse($isValid);
+        }
+    }
+
+    public function dataForStringEndWith() {
+        return [
+            [true, 'hello, world!', 'rld!'],
+            [false, 'hello, world!', 'Rld!'],
+        ];
+    }
+
+    /**
+     * @param bool   $isValid
+     * @param string $str
+     * @param string $end
+     *
+     * @dataProvider dataForStringEndWithCIMore
+     */
+    public function testStringEndWithCIMore(bool $isValid, string $str, string $end) {
+        try {
+            (new StringValidator($str))
+                ->endWithCI($end)
+                ->validate();
+            static::assertTrue($isValid);
+        } catch (ValidatorException $e) {
+            static::assertFalse($isValid);
+        }
+    }
+
+    public function dataForStringEndWithCIMore() {
+        return [
+            [true, 'Good morning', 'MorNing'],
+            [false, 'Good morning', 'good'],
+        ];
+    }
+
+    public function testStringEndWithCI() {
+        $vars = XTS::validator($this->testData)
+            ->str('string')
+            ->endWithCI('WORld!')
+            ->validate()
+            ->safeVar;
+        static::assertEquals($this->testData['string'], $vars['string']);
+    }
+
+    /**
+     * @expectedException \x2ts\validator\ValidatorException
+     */
+    public function testStringCallbackFailed() {
+        XTS::validator($this->testData)
+            ->str('string')
+            ->callback(function ($var) {
+                return false;
+            })->validate();
+    }
+
+    /**
+     * @param bool   $isValid
+     * @param string $str
+     * @param array  $enum
+     * @param bool   $strict
+     * @dataProvider dataForStringInEnum
+     */
+    public function testStringInEnum(bool $isValid, string $str, array $enum, bool $strict) {
+        try {
+            (new StringValidator($str))
+                ->inEnum($enum, $strict)
+                ->validate();
+            self::assertTrue($isValid);
+        } catch (ValidatorException $e) {
+            self::assertFalse($isValid);
+        }
+    }
+
+    public function dataForStringInEnum() {
+        return [
+            [true, 'abc', ['abc', 'def'], true],
+            [false, 'abc', ['ABC', 'def'], true],
+            [true, '123', [123, 321], false],
+            [false, '123', [123, 321], true],
+        ];
+    }
+
+    /**
+     * @expectedException \x2ts\validator\ValidatorException
+     */
+    public function testInvalidStringAlready() {
+        XTS::validator($this->testData)
+            ->str('zeroInt')
+            ->contain('abc')
+            ->containCI('ABC')
+            ->match('/^$/')
+            ->notMatch('/#\@#/')
+            ->startWith('great')
+            ->startWithCI('Great')
+            ->endWith('Die')
+            ->endWithCI('Die')
+            ->callback(function () { return false; })
+            ->inEnum(['abc', 'def'])
+            ->length(18)
+            ->validate();
     }
 
     public function testTwoErrorMessages() {
